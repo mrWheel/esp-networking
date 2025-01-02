a0566754878fb75bed9cfa730ea3bed8b200225e
Willem Aandewiel <Willem@Aandewiel.nl>
2025-01-02 13:54:03 +0100

    Adding keywords.txt

--- examples/example1.cpp
+++ examples/example1.cpp
@@ -1,10 +1,13 @@
+
 #include "Networking.h"
 
 Networking* networking = nullptr;
 Stream* debug = nullptr;
+
 uint32_t loopCount = 0;
 
-void setup() {
+void setup() 
+{
     delay(5000);
     
     networking = new Networking();
@@ -14,7 +17,8 @@ void setup() {
     debug = networking->begin("esp32", 0, Serial, 115200);
     #endif
     
-    if (!debug) {
+    if (!debug) 
+    {
         #ifdef ESP8266
         ESP.restart();
         #else
@@ -23,28 +27,34 @@ void setup() {
     }
     
     // Example of using the IP methods
-    if (networking->isConnected()) {
+    if (networking->isConnected()) 
+    {
         debug->print("Device IP: ");
         debug->println(networking->getIPAddressString());
     }
 }
 
-void loop() {
+void loop() 
+{
     networking->loop();
     
-    // Your main code here
+    //-- Your main code here
     delay(1000);
     debug->printf("Hello, world! IP: %s, loopCount[%d]\n",
                  networking->getIPAddressString().c_str(),
                  loopCount++);
     
-    // Example: Periodically check connection status
+    //-- Example: Periodically check connection status
     static unsigned long lastCheck = 0;
-    if (millis() - lastCheck > 30000) {  // Every 30 seconds
-        if (networking->isConnected()) {
+    if (millis() - lastCheck > 30000) //-- Every 30 seconds
+    {  
+        if (networking->isConnected()) 
+        {
             debug->print("Still connected. IP: ");
             debug->println(networking->getIPAddressString());
-        } else {
+        } 
+        else 
+        {
             debug->println("WiFi connection lost!");
         }
         lastCheck = millis();
